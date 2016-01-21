65e9e7079c71c62812a2c44c0e367906a690883e
poc.hsu <poc7667@gmail.com>
2016-01-21 15:29:27 +0800

    add web api for recommendations

--- app/controllers/api/v1/recommendations_controller.rb
+++ app/controllers/api/v1/recommendations_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::RecommendationsController < ApplicationController
   def index
+    @recommendations = Recommender.get_recommendations(params["user_id"].to_i, params["strategy"])
+    respond_to do |format|
+      format.json
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  namespace :api do
-  namespace :v1 do
-    get 'recommendations/index'
+  namespace :api, defaults: {format: 'json'} do
+    namespace :v1 do
+      get 'recommendations/:user_id/:strategy/' => "recommendations#index"
     end
   end
 
